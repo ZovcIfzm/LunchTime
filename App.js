fd137e60f04aaf89c0e9d522ca2731d50e0e5b3e
Alex Ruan <alexruan2222@gmail.com>
2020-05-23 13:44:02 -0400

    created expo-tabs template and implemented redux

---
+++ App.js
@@ -0,0 +1,56 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+
+import useCachedResources from './hooks/useCachedResources';
+import BottomTabNavigator from './navigation/BottomTabNavigator';
+import LinkingConfiguration from './navigation/LinkingConfiguration';
+
+const Stack = createStackNavigator();
+
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+
+const initialState = {
+  counter : 0
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type){
+    case 'INCREASE_COUNTER':
+      return{counter:state.counter+1}
+  }
+  return state
+}
+
+const store = createStore(reducer);
+
+export default function App(props) {
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      
+      <Provider store = {store}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
+          <NavigationContainer linking={LinkingConfiguration}>
+            <Stack.Navigator>
+              <Stack.Screen name="Root" component={BottomTabNavigator} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
